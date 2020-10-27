37c85f527671c5609a07a224471962765cf01c42
Gilberto Bermúdez Garro <gbermude@outlook.com>
2020-10-26 18:53:32 -0600

    Actualización de ejercicio del día

---
+++ 20201026/WA50/WA50/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Northwind.Store.Data;
+using Northwind.Store.Model;
+using WA50.Models;
+using WA50.ViewModels;
+
+namespace WA50.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// /Home/Index
+        /// </summary>
+        //public IActionResult Index(string filter = "")
+        public IActionResult Index(ProductIndexViewModel vm)
+        {
+            HttpContext.Session.SetString(nameof(vm.Filter), vm.Filter ?? "");
+            var filter = HttpContext.Session.GetString(nameof(vm.Filter));
+            
+            var result = new List<Product>();
+            using (var db = new NWContext())
+            {
+                //result = db.Products.Where(p=> p.ProductName.Contains(filter)).ToList();
+                result = db.Products.Where(p => p.ProductName.Contains(vm.Filter)).ToList();
+            }
+
+            //var vm = new ProductIndexViewModel() { Products = result };
+            vm.Products = result;
+
+
+            // Dictionary
+            //ViewData["products"] = result;
+            //ViewBag.productsD = result;
+
+            return View(vm);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 20201026/WA50/WA50/ViewModels/ProductIndexViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Northwind.Store.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WA50.ViewModels
+{
+    public class ProductIndexViewModel
+    {
+        public string Filter { get; set; }
+        public List<Product> Products { get; set; }
+
+        public int TotalCount { get; set; }
+        public int PageSize => 5;
+        public int PageCount => TotalCount / PageSize;
+
+        /// <summary>
+        /// Página actual
+        /// </summary>
+        public int Page { get; set; }
+    }
+}
