db241efa1f1544c74d0fea8d041db0bbe0cdcbbd
Gilberto Bermúdez Garro <gbermude@outlook.com>
2020-11-02 19:37:46 -0600

    Ejemplo de Auth

---
+++ proyecto/NorthwindStore/Northwind.Store.UI.Intranet/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Northwind.Store.UI.Intranet.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.HasDefaultSchema("Auth");
+        }
+    }
+}
---
+++ proyecto/NorthwindStore/Northwind.Store.UI.Intranet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Northwind.Store.UI.Intranet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
