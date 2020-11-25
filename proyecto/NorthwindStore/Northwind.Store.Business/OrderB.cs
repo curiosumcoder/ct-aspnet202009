ead2ff731c596519853d17bf35c2429da68e6b4a
Gilberto Bermúdez Garro <gbermude@outlook.com>
2020-11-25 15:24:05 -0600

    Actualización

---
+++ proyecto/NorthwindStore/Northwind.Store.Business/OrderB.cs
@@ -0,0 +1,51 @@
+﻿using Northwind.Store.Data;
+using Northwind.Store.Model;
+using Northwind.Store.Notification;
+using System;
+using System.Threading.Tasks;
+
+namespace Northwind.Store.Business
+{
+    public class OrderB
+    {
+        private readonly BaseRepository<Order, int> _oR = null;
+        public OrderB(BaseRepository<Order, int> oR)
+        {
+            _oR = oR;
+        }
+
+        public async Task Create(Order o, Notifications nm = null)
+        {
+            // Validaciones y acciones sobre otras tablas
+            if (nm != null)
+            {
+                if (o.OrderDetails.Count > 0)
+                {
+                    Message m = new Message()
+                    {
+                        Title = "La orden no contiene detalle."
+                    };
+                    nm.Add(m);
+                }
+
+                if (o.State != ModelState.Added)
+                {
+                    Message m = new Message()
+                    {
+                        Title = "La order no viene en el correcto estado"
+                    };
+                    nm.Add(m);
+                }
+
+                if (nm.Count == 0)
+                {
+                    await _oR.Save(o, nm);
+                }
+            }
+            else
+            {
+                await _oR.Save(o, nm);
+            }
+        }
+    }
+}
---
+++ proyecto/NorthwindStore/Northwind.Store.Model/Metadata/Order.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Northwind.Store.Model
+{
+    //[ModelMetadataType(typeof(OrderMetadata))]
+    public partial class Order : ModelBase
+    {
+
+    }
+}
