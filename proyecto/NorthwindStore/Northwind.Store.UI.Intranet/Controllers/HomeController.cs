ced3f6483cf254e1d989ce6aaabdedd35d1ee2a8
Gilberto Bermúdez Garro <gbermude@outlook.com>
2020-12-14 16:06:20 -0600

    Revert "Actualización"
    This reverts commit f7a922b6e7fc9f0da7e3840dc84a82ab693cb9d8.

---
+++ proyecto/NorthwindStore/Northwind.Store.UI.Intranet/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Northwind.Store.UI.Intranet.Models;
+
+namespace Northwind.Store.UI.Intranet.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //throw new ApplicationException("Esta es una prueba!");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            //return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+            var requestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            var exceptionMessage = "";
+
+            var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            if (exceptionHandlerPathFeature?.Error is ApplicationException)
+            {
+                var ex = exceptionHandlerPathFeature?.Error;
+
+                exceptionMessage = $"Error en la aplicación: {ex.Message}";                
+                _logger.LogError(exceptionMessage);
+            }
+            if (exceptionHandlerPathFeature?.Path == "/")
+            {
+                exceptionMessage += " desde la raíz.";
+            }
+
+            return View("Error", new ErrorViewModel { RequestId = requestId });
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult ErrorWithCode(string code)
+        {
+            var requestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            var errorStatusCode = code;
+            var originalURL = "";
+
+            var statusCodeReExecuteFeature = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+            if (statusCodeReExecuteFeature != null)
+            {
+                originalURL =
+                    statusCodeReExecuteFeature.OriginalPathBase
+                    + statusCodeReExecuteFeature.OriginalPath
+                    + statusCodeReExecuteFeature.OriginalQueryString;
+            }
+
+            return View("Error", new ErrorViewModel { RequestId = requestId });
+        }
+    }
+}
---
+++ proyecto/NorthwindStore/Northwind.Store.UI.Intranet/wwwroot/js/forms.js
@@ -0,0 +1,16 @@
+﻿// Se crea el manejador el evento para cuando se efectuan cambios de datos
+let inputs = document.querySelectorAll('.form-control');
+inputs.forEach(input => {
+    input.addEventListener('change', (event) => {
+        let prop = event.target.name;
+        // Se agrega evidencia del cambio para ser enviado como parte del POST
+        // Se confirma que no se haya agregado previamente
+        if (!document.querySelector(`input[type='hidden'][value='${prop}']`)) {
+            let input = document.createElement('input');
+            input.type = 'hidden';
+            input.name = 'ModifiedProperties';
+            input.value = prop;
+            event.target.parentElement.appendChild(input);
+        }
+    });
+});
